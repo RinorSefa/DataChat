aeb3ef146b03ead9fa78f466c39d20c1cf28e06f
@sefari <rinor.sefa@uzh.ch>
2023-05-24 13:06:05 +0200

    Refactored user input retrieval from hardcoded values to command-line interface (CLI) input

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import os
 # Get the value of an environment variable
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-# create a chat completion
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+while True:
+    question = input("What is your question to OpenAI (type 'EXIT' to quit): ")
 
-# print the chat completion
-print(chat_completion.choices[0].message.content)
+    if question.upper() == "EXIT":
+        break
+
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": question}]
+    )
+    print(chat_completion.choices[0].message.content)
