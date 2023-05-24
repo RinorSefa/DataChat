46ee31b95c1c6ee84dce0db527fa5249d6b4785b
@sefari <rinor.sefa@uzh.ch>
2023-05-24 14:04:34 +0200

    Added the message API to include the CSV data

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os
 # Get the value of an environment variable
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+with open('data/5_spend_dataset_current.csv', 'r') as file:
+    # Read the entire contents of the file
+    contents = file.read()
+
 while True:
     question = input("What is your question to OpenAI (type 'EXIT' to quit): ")
 
@@ -12,6 +16,10 @@ while True:
 
     chat_completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": question}]
+        messages=[
+            {"role": "system", "content": "You will answer question regarding a CSV file content"},
+            {"role": "user", "content": " Here is the CSV data " + contents},
+            {"role": "user", "content": question}
+        ]
     )
     print(chat_completion.choices[0].message.content)
