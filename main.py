c9059276122c2c4337d4ff90329d2165f985cb3f
@sefari <rinor.sefa@uzh.ch>
2023-05-24 15:06:48 +0200

    Refactored file location handling to support user-provided paths via arguments

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import openai
 import os
+import sys
 
 # Get the value of an environment variable
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-with open('data/5_spend_dataset_current.csv', 'r') as file:
-    # Read the entire contents of the file
-    contents = file.read()
+# Check if the file location parameter is provided
+if len(sys.argv) < 2:
+    print("Please provide the exact file location as a parameter.")
+    print("Usage: python main.py <file_location> ")
+    sys.exit(1)
+
+# Retrieve the file location parameter
+fileLocation = sys.argv[1]
+
+try:
+    # Open the file in read mode
+    with open(fileLocation, 'r') as file:
+        # Read the entire contents of the file
+        contents = file.read()
+
+except FileNotFoundError:
+    print("Error: File not found.")
+    print("Please make sure the file exists and provide the correct file location.")
+    sys.exit(1)
+
+except IOError:
+    print("Error: Unable to read the file.")
+    print("Please check if the file is accessible and not corrupted.")
+    sys.exit(1)
 
 while True:
     question = input("What is your question to OpenAI (type 'EXIT' to quit): ")
